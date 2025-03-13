b9845c44a8b9f0ba1ed429bb01543b64b94c008c
zhai-dada <zhaipengwei0728@gmail.com>
2025-03-13 18:36:33 +0800

    开始制作编译器，简单的return

---
+++ Makefile
@@ -0,0 +1,12 @@
+CFLAGS=-std=c11 -g -static
+
+stupidcc:stupidcc.c
+	gcc $(CFLAGS) $< -o $@
+
+test:stupidcc
+	./test.sh
+
+clean:
+	rm -f stupidcc *.o *~ tmp*
+
+.PHONY: test clean
---
+++ stupidcc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "参数不足\n");
+        return 1;
+    }
+
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("  mov $%d, %%rax\n", atoi(argv[1]));
+    printf("  ret\n");
+    return 0;
+}
